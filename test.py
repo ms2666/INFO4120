e69af831025b84d99c1a4f510da6c9ccc0dcccb3
Hane Aung <ms2666@cornell.edu>
2017-05-01 17:05:38 -0400

    updated test.py

--- test.py
+++ test.py
@@ -21,40 +21,54 @@ from keras import callbacks
 
 # scipy stuff
 from scipy.interpolate import interp1d
+from scipy.stats import mode
 
 # preprocessing stuff
 from preprocessingTR import *
 
-def load_data(xpath='./Data_test/processed/full.pickle', ypath='./Data_test/processed/labels.npy'):
-	return pd.read_pickle(xpath), np.load(ypath)
+def load_data(xpath='./Data_test/processed/full.pickle'):
+    return pd.read_pickle(xpath)
 
-def scale_data(df, labels, model_dir='./Models/'):
-	"""
-	Scale data, convert to 0 indexing
-	"""
-	xTe = df.values
-	num_classes = int(labels.max())
-	ss = joblib.load(model_dir + 'ss.pkl')
-	xTe = ss.transform(xTe)
+def scale_data(df, model_dir='./Models/'):
+    """
+    Scale data
+    """
+    xTe = df.values
+    num_classes = int(labels.max())
+    ss = joblib.load(model_dir + 'ss.pkl')
+    xTe = ss.transform(xTe)
 
-	# zero index labels
-	yTe = np.mod(labels, num_classes)
-
-	return xTe, yTe
+    return xTe
 
 if __name__ == '__main__':
-	print('Executing testing script...')
+    print('Executing testing script...')
 
-	# generate user dictionary
-	u_dict = generate_udict('./Data_test/')
+    # generate user dictionary
+    u_dict = generate_udict('./Data_test/')
 
-	# preprocess and save training data
-	preprocess_and_save(u_dict, dir_name='./Data_test/processed/')
+    # preprocess and save training data
+    preprocess_and_save(u_dict, dir_name='./Data_test/processed/')
 
-	# merge and save files as binary objects for quick loading
-	merge_incremental(base='./Data_test/processed/')
+    # merge and save files as binary objects for quick loading
+    merge_incremental(base='./Data_test/processed/')
 
-	# load and split data
-	data, labels = load_data()
-	xTe, yTe = scale_data(data, labels)
-	print(xTe, yTe)
+    # load and split data
+    data = load_data()
+    xTe = scale_data(data)
+    
+    print('Reshaping data')
+    xTe_conv = xTe.reshape(-1, 3, 300, 1)
+    
+    print('Loading model')
+    model = keras.models.load_model('./Models/ConvNetC.h5')
+    
+    preds = model.predict(xTe_conv)
+    
+    user_lookup = {0: 'other', 1: 'Mukund', 2: 'Frank'}
+    
+    id_predicted = mode(preds.round().argmax(axis=1)).mode[0]
+    
+    if id_predicted == 0:
+        print('Get the fuck out of here!')
+    else:
+        print('Welcome back %s!' % user_lookup[id_predicted])
